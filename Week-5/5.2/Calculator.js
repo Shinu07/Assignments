141e2a5239d46b6613fe58df0f37d0ee6d808708
Soumya ranjan Purohit <Official.soumyaranjan.07@gmail.com>
2024-12-05 02:35:49 +0530

    optimized solution

--- Week-5/5.2/Calculator.js
+++ Week-5/5.2/Calculator.js
@@ -8,35 +8,62 @@ Check if it works with/without express.json middleware
 
 */
 
-const express = require ('express')
+const express = require('express')
 
-    const app = express()
+const app = express()
 
-    app.use(express.json())
+app.use(express.json())
 
 
-    // middleware to validate the function 
+// middleware to validate the function 
 
-    function validateInput(req,res,next){
-        const { a, b } = req.body
+function validateInput(req, res, next) {
+    const { a, b } = req.body
 
-        console.log(isNaN(a),isNaN(b));
-        
-        if(!a || !b){
-            res.status(400).send({
-                error: "Please provide value for a and b"
-            })
-        } else if(isNaN(a) || isNaN(b)){
-            res.status(400).send({
-                error: "Please provide valid numbers for a and b",
-            });
-        }else{
-            next()
-        }
+    console.log(isNaN(a), isNaN(b));
+
+    if (!a || !b) {
+        res.status(400).send({
+            error: "Please provide value for a and b"
+        })
+    } else if (isNaN(a) || isNaN(b)) {
+        res.status(400).send({
+            error: "Please provide valid numbers for a and b",
+        });
+    } else {
+        next()
     }
+}
 
-    // this middleware used for all routes
-    app.use(validateInput)
+// this middleware used for all routes
+app.use(validateInput)
+
+// optimized way to do it
+const calculateres = (operaion) => (req, res) => {
+    const { a, b } = req.body
+
+    const opertors = {
+        sum: (x, y) => x + y,
+        subtract: (x, y) => x - y,
+        multiply: (x, y) => x * y,
+        divide : (x,y)=> {
+            if(y === 0 ){
+                res.status(400).json({error: "cannot divide by zero"})
+            }
+            return x/y
+        }}
+
+        const resx = opertors[operaion](a,b)
+        return res.json({resx})
+}
+
+// Routes
+app.post("/sum",validateInput,calculateres('sum'))
+app.post("/sum",validateInput,calculateres('divide'))
+app.post("/sum",validateInput,calculateres('multiply'))
+app.post("/sum",validateInput,calculateres('subtract'))
+
+/*
 
     app.post("/sum",(req,res)=>{
         const { a, b } = req.body
@@ -46,7 +73,7 @@ const express = require ('express')
         })
     })
 
-    app.post("/ssubtrac",(req,res)=>{
+    app.post("/subtrac",(req,res)=>{
 
         const { a, b } = req.body
         res.send({
@@ -73,5 +100,5 @@ const express = require ('express')
 
     })
 
-
-    app.listen(3000)
+*/
+app.listen(3000)
